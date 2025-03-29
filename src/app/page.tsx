e67249c8a701b7240e875826fef80bff451a2f65
bulaonjohnemm4nuel.21@gmail.com <bulaonjohnemm4nuel.21@gmail.com>
2025-03-29 20:04:54 +0800

    page.tsk: created log in form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div>
+    <div className="container bg-gray-400">
       
+      <div className="bg-maincolor text-[#45adff]"> T H I N K I F Y</div>
+      {/* LOG IN FORM */}
+      <div className="container flex justify-center">
+      <form className="bg-maincolor" action="">
+        <label htmlFor="email">Email: </label>
+        <input className="" type="email" name="email" id="email" required />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" required />
+        <br />
+        <button type="submit">Log in</button>
+      </form>
+      </div>
     </div>
   );
 }
